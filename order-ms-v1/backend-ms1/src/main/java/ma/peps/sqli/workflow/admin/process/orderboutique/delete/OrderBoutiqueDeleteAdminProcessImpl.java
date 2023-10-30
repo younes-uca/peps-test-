77df27ad9ed157328413981fc79ae756bf41846d
younes-uca <y.zouani@uca.ma>
2023-10-30 12:32:11 +0100

    fix bug: no auth required and api of save

--- order-ms-v1/backend-ms1/src/main/java/ma/peps/sqli/workflow/admin/process/orderboutique/delete/OrderBoutiqueDeleteAdminProcessImpl.java
+++ order-ms-v1/backend-ms1/src/main/java/ma/peps/sqli/workflow/admin/process/orderboutique/delete/OrderBoutiqueDeleteAdminProcessImpl.java
@@ -1,23 +1,18 @@
 package ma.peps.sqli.workflow.admin.process.orderboutique.delete;
 
-import ma.peps.sqli.bean.core.commun.OrderStatus;
 import ma.peps.sqli.bean.core.order.OrderBoutique;
-import ma.peps.sqli.service.facade.admin.commun.OrderStatusAdminService;
 import ma.peps.sqli.service.facade.admin.order.OrderBoutiqueAdminService;
 import ma.peps.sqli.workflow.admin.process.orderboutique.OrderBoutiqueConstant;
-import ma.peps.sqli.workflow.admin.process.orderboutique.save.OrderBoutiqueSaveAdminInput;
 import ma.peps.sqli.zynerator.process.AbstractProcessImpl;
 import ma.peps.sqli.zynerator.process.Result;
-
-import java.time.LocalDateTime;
 
 public class OrderBoutiqueDeleteAdminProcessImpl extends AbstractProcessImpl<OrderBoutiqueDeleteAdminInput, OrderBoutiqueDeleteAdminOutput, OrderBoutique, OrderBoutiqueDeleteAdminConverter> implements OrderBoutiqueDeleteAdminProcess {
 
     @Override
     public void init(OrderBoutiqueDeleteAdminInput input, OrderBoutique item) {
-        OrderBoutique byId = service.findById(input.getId());
-        if (byId != null) {
-            item.setOrderStatus(byId.getOrderStatus());
+        OrderBoutique orderBoutiqueByReference = service.findByReferenceEntity(item);
+        if (orderBoutiqueByReference != null) {
+            item.setOrderStatus(orderBoutiqueByReference.getOrderStatus());
         }
     }
 
@@ -31,6 +26,7 @@ public class OrderBoutiqueDeleteAdminProcessImpl extends AbstractProcessImpl<Ord
     @Override
     public void run(OrderBoutiqueDeleteAdminInput input, OrderBoutique t, Result<OrderBoutiqueDeleteAdminInput, OrderBoutiqueDeleteAdminOutput, OrderBoutique> result) {
         service.deleteWithAssociatedLists(t);
+        service.deleteById(t.getId());
     }
 
 
