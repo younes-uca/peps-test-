384922be8736984a05574989a78685d887a773d6
younes-uca <y.zouani@uca.ma>
2023-10-30 11:51:29 +0100

    fix bug: no auth required and api of save

--- order-ms-v1/backend-ms1/src/main/java/ma/peps/sqli/workflow/admin/process/orderboutique/save/OrderBoutiqueSaveAdminInjector.java
+++ order-ms-v1/backend-ms1/src/main/java/ma/peps/sqli/workflow/admin/process/orderboutique/save/OrderBoutiqueSaveAdminInjector.java
@@ -2,6 +2,7 @@ package ma.peps.sqli.workflow.admin.process.orderboutique.save;
 
 import ma.peps.sqli.service.facade.admin.commun.OrderStatusAdminService;
 import ma.peps.sqli.service.facade.admin.order.OrderBoutiqueAdminService;
+import ma.peps.sqli.service.facade.admin.order.OrderLineAdminService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class OrderBoutiqueSaveAdminInjector {
 
     @Bean
-    public OrderBoutiqueSaveAdminProcess orderBoutiqueSaveAdmin(OrderBoutiqueAdminService service, OrderStatusAdminService orderStatusAdminService, OrderBoutiqueSaveAdminConverter converter) {
-        return new OrderBoutiqueSaveAdminProcessImpl(service, orderStatusAdminService, converter);
+    public OrderBoutiqueSaveAdminProcess orderBoutiqueSaveAdmin(OrderBoutiqueAdminService service, OrderLineAdminService orderLineService, OrderStatusAdminService orderStatusAdminService, OrderBoutiqueSaveAdminConverter converter) {
+        return new OrderBoutiqueSaveAdminProcessImpl(service, orderLineService, orderStatusAdminService, converter);
     }
 
 }
